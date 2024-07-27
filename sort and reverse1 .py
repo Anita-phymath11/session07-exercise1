e62d6db3f99c679f4371ab1cb772997f88526b08
Anita-phymath11 <a90k.karami@gmail.com>
2024-07-28 00:15:17 +0330

    Add files via upload

---
+++ sort and reverse1 .py
@@ -0,0 +1,10 @@
+
+lst1=[]
+for i in range(5):
+   num=int(input("enter your number: ")) 
+   lst1.append(num)
+lst1.sort()
+print("Ascending: ",lst1)
+lst1.reverse()
+print("Descending: ",lst1)
+print(":))))))))))))))))))))))))))))))))))))))))))byeeeee")
