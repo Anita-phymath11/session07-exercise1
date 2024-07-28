2436c9476f8a9f24dba022c72ec10ca42cc81f5f
Anita-phymath11 <a90k.karami@gmail.com>
2024-07-28 13:00:44 +0330

    Update sort and reverse1 .py

--- sort and reverse1 .py
+++ sort and reverse1 .py
@@ -1,4 +1,9 @@
-
+#what does this program do?
+#This program takes 5 numbers 
+# from the user and
+# shows them in ascending
+# and descending order.
+#........................................
 lst1=[]
 for i in range(5):
    num=int(input("enter your number: ")) 
